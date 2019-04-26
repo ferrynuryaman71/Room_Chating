a60e8186f54a5962612cbb09dda0a92facc105dd
ferry <ferrynuryaman01@gmail.com>
2019-04-26 19:27:29 +0700

    add connection and disconnetion

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 1998;
 var app = express();
+const server = http.createServer(app)
+const io = socketIO(server)
 
 app.use(express.static(publicPath));
 
-app.listen(1998, () => {
-	console.log("connect to port 1998");
+io.on('connection', (socket) => {
+	console.log('new connection')
+})
+
+server.listen(1998, () => {
+	console.log(`connect to port ${port}`);
 });
